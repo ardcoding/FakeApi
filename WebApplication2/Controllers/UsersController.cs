ca353ff19a378915db4089164efa442091781a4c
Ahmet Rüchan Dora <ahmetdoraa@gmail.com>
2023-07-26 10:02:14 +0300

    FakeApi

---
+++ WebApplication2/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Fake;
+using WebApplication2.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/<UsersController>
+
+
+        private List<User> _users = FakeData.GetUsers();
+        [HttpGet]
+        public List<User> Get()
+        {
+            return _users;
+        }
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            var user  = _users.FirstOrDefault(x => x.Id == id);
+            return user;
+        }
+    }
+}
---
+++ WebApplication2/Fake/FakeData.cs
@@ -0,0 +1,24 @@
+﻿using Bogus;
+using WebApplication2.Models;
+
+namespace WebApplication2.Fake
+{
+    public class FakeData
+    {
+        private static List<User> _users;
+
+        public static List<User> GetUsers()
+        {
+            if (_users == null)
+            {
+                _users = new Faker<User>()
+                    .RuleFor(u => u.Id, f => f.IndexFaker + 1)
+                    .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+                    .RuleFor(u => u.LastName, f => f.Name.LastName())
+                    .RuleFor(u => u.Address, f => f.Address.City())
+                    .Generate(20);
+            }
+            return _users;
+        }
+    }
+}
